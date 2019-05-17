fce079987ea70f09f494ca21fd0031e78dd14c8c
Otacilio Siqueira <ohs@cesar.school>
2019-05-17 13:24:15 -0300

    Update speech-test

--- speech-test/speech-test.py
+++ speech-test/speech-test.py
@@ -1,13 +1,16 @@
 import speech_recognition as sr
 
 r = sr.Recognizer()
-audioF = 'Work.wav'
+audio_path = 'teste_audio.wav'
 
-with sr.AudioFile(audioF) as source:
+with sr.AudioFile(audio_path) as source:
+
     audio = r.record(source)
-    print("Done!")
+    print("Convertendo ...")
+
     try:
         text = r.recognize_google(audio, language = 'pt-BR')
         print(text)
+
     except Exception as e:
-        print("Sorry could not recognize what you said")
+        print(e)
